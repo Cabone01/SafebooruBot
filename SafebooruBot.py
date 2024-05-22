54ea50360b8d56131febc92ad3b3973dd787125b
Andres <cabone01@gmail.com>
2024-05-21 23:58:10 -0400

    Setting limit functioning, channel table developed, searching nearly completed

--- SafebooruBot.py
+++ SafebooruBot.py
@@ -17,7 +17,7 @@ from discord.utils import find
 from discord.ext import tasks
 
 import mysql.connector
-from important import ep, user, ps
+from important import ep, user, ps, host, user_2, password
 
 intents = discord.Intents.default()
 intents.members = True
@@ -36,9 +36,9 @@ bot = commands.Bot(command_prefix='!!', intents=intents)
 @bot.event
 async def on_guild_join(guild):
     cnx = mysql.connector.connect(
-    host=ep,
-    user=user,
-    password=ps
+    host=host,
+    user=user_2,
+    password=password
     )
 
     if cnx.is_connected():
@@ -48,11 +48,14 @@ async def on_guild_join(guild):
         
         cursor.execute(f"USE server_{guild.id};")
         cursor.execute("DROP TABLE IF EXISTS settings;")
-        cursor.execute("Create TABLE settings(ID INT NOT NULL AUTO_INCREMENT, Limit INT,  In_process TINYINT(1));")
+        cursor.execute("Create TABLE settings(ID INT NOT NULL AUTO_INCREMENT, List_limit INT,  In_process TINYINT(1), PRIMARY KEY (ID));")
+        row = [10, 0]
+        cursor.execute("INSERT INTO settings (List_limit, In_process) VALUES (%s, %s)", row)
         # Perhaps a row added in once the setting is finalized
         cursor.execute("DROP TABLE IF EXISTS channels;")
-        cursor.execute("Create TABLE channels(Row_ID INT NOT NULL AUTO_INCREMENT, Channel_ID VARCHAR(32), Name VARCHAR(48), Amount_of_tags_selected INT);")
+        cursor.execute("Create TABLE channels(Row_ID INT NOT NULL AUTO_INCREMENT, Channel_ID VARCHAR(32), Name VARCHAR(48), Amount_of_tags_selected INT, PRIMARY KEY (Row_ID));")
         cursor.close()
+    cnx.commit()
     cnx.close()
 
 
@@ -66,9 +69,9 @@ async def on_ready():
 @bot.command(help='Sets the current channel as a location for images to be sent to')
 async def setArtThread(ctx):
     cnx = mysql.connector.connect(
-    host=ep,
-    user=user,
-    password=ps
+    host=host,
+    user=user_2,
+    password=password
     )
     channel_id = "channel_" + str(ctx.channel.id)
 
@@ -81,7 +84,7 @@ async def setArtThread(ctx):
         if channel_id in table_names:
             await ctx.send("Silly. This channel is already a designated place for art")
         else:
-            cursor.execute(f"Create TABLE {channel_id}(ID INT NOT NULL AUTO_INCREMENT, Tag VARCHAR(96), Category VARCHAR(96), Link VARCHAR(192), Latest_art VARCHAR(192));")
+            cursor.execute(f"Create TABLE {channel_id}(Id INT NOT NULL AUTO_INCREMENT, Tag VARCHAR(96), Category VARCHAR(96), Link VARCHAR(192), Latest_art VARCHAR(192), PRIMARY KEY(Id));")
             sql = "INSERT INTO channels VALUES (%s, %s, %s)"
             row = (ctx.channel.id, ctx.channel.name, 0)
             cursor.execute(sql, row)
@@ -95,11 +98,11 @@ async def setArtThread(ctx):
 
 @bot.command()
 async def setLimit(ctx):
-    #cnx = mysql.connector.connect(
-    #host=ep,
-    #user=user,
-    #password=ps
-    #)
+    cnx = mysql.connector.connect(
+    host=host,
+    user=user_2,
+    password=password
+    )
     
     number = ctx.message.content[10:].strip()
 
@@ -107,29 +110,118 @@ async def setLimit(ctx):
         number = int(number)
         
         if number <= 25:
-            #if cnx.is_connected():
-            #    cursor = cnx.cursor()
-            #    cursor.execute(f"USE server_{ctx.guild.id};")
-            #    sql = f"UPDATE settings SET Limit = {number} WHERE ID = 1"
-            #    cursor.execute(sql)
-            #    cnx.commit()
+            if cnx.is_connected():
+                cursor = cnx.cursor()
+                cursor.execute(f"USE server_{ctx.guild.id};")
+                sql = f"UPDATE settings SET List_limit = {number} WHERE ID = 1"
+                cursor.execute(sql)
+                cnx.commit()
 
-            #    cursor.close()
+                cursor.close()
             await ctx.send(f"The limit for the search command is now set to {number}")
         else:
             await ctx.send(f"{number} is too high of a value. Please keep it at 25 or below")
 
-        #cnx.close()
+        cnx.close()
     else:
         await ctx.send(f"There are letters and/or symbols following your command. Can you use only numeric values please?")                       
 
-    
-    #cnx.close()
-    #await ctx.send(f"{len(ctx.message.content[10:].strip())}")
-
 @bot.command()
 async def search(ctx):
-    await ctx.send("")
+
+    msg = ctx.message.content[8:].strip().split("|")
+    name = []
+    cat = []
+    c_type = []
+
+    error_text = []
+
+    if len(msg) > 1:
+        if len(msg) > 2:
+            name, cat, c_type = [i.lower().split() for i in msg]
+        else:
+            name, cat = [i.lower().split() for i in msg]
+    else:
+        name.append(msg[0])
+
+    if len(name) > 24:
+        error_text.append(f"You exceeded the amount of words for the **name** by {len(name) - 24}. Make sure to use at most 24 words when searching with the **name**")
+    if len(cat) > 4:
+        error_text.append(f"You exceeded the amount of words for the **category** by {len(cat) - 4}. Make sure to use at most 4 words when searching with the **category**")
+    if len(c_type) > 1:
+        error_text.append(f"You exceeded the amount of words for the **type** by {len(c_type) - 1}. Make sure to use only 1 word when searching the **type**")
+
+    if len(error_text) > 0:
+        await ctx.send('\n \n'.join(error_text))
+    else:
+        search_name = []
+        slct_name = []
+        search_cat = []
+        slct_cat = []
+        search_type = ""
+        slct_type = ""
+        
+        srch = []
+        slct = []
+
+        if len(name) > 0:
+            col_value = 1
+            for i in name:
+                # This line below would be too intensive for a database that I intend to be useable in multiple servers and pushed to cloud. Keeping it for future refrence on improvement
+                # search_name.append(f"LOWER({i}) in (Name_1, Name_2, Name_3, Name_4, Name_5, Name_6, Name_7, Name_8, Name_9, Name_10, Name_11, Name_12, Name_13, Name_14, Name_15, Name_16, Name_17, Name_18, Name_19, Name_20, Name_21, Name_22, Name_23, Name_24)")
+                srch.append(f"'{i}' LIKE Name_{col_value}")
+                slct.append(f"Name_{col_value}")
+                col_value += 1
+        if len(cat) > 0:
+            col_value = 1
+            for i in name:
+                srch.append(f"'{i}' LIKE Category_{col_value}")
+                slct.append(f"Category_{col_value}")
+                col_value += 1
+        if len(c_type) > 0:
+            srch.append(f"'{c_type[0]}' LIKE Main_Category")
+            slct.append("Main_Category")
+        
+       
+        cnx = mysql.connector.connect(
+        host=host,
+        user=user_2,
+        password=password
+        )
+
+        fnd_tags = []
+        if cnx.is_connected():
+                cursor = cnx.cursor()
+                cursor.execute(f"USE server_{ctx.guild.id};")
+                sql = f"SELECT List_limit FROM settings"
+                cursor.execute(sql)
+                limit = cursor.fetchone()[0]
+                print(limit)
+
+                cursor.execute(f"USE safebooru_info;")
+                sql = f"SELECT * FROM safebooru_tags WHERE {' OR '.join(srch)} LIMIT {limit}"
+                cursor.execute(sql)
+                tags = cursor.fetchall()
+                print(tags)
+                list_tags = []
+                for i in tags:
+                    k = [j for j in i if j != None]
+                    
+                    list_tags.append(' '.join(k))
+
+                fnd_tags = '\n \n'.join(list_tags)
+
+                cursor.close()
+        cnx.close()
+
+        
+        #await ctx.send(f"{name}, {cat}, {c_type}")
+        #await ctx.send(f"{', '.join(slct)} \n {' OR '.join(srch)}")
+        await ctx.send(fnd_tags)
+
+# SELECT *
+# FROM testing 
+# WHERE 'foo' in (col1, col2, col3, . . . );
 
 @bot.command()
 async def select(ctx):
@@ -145,53 +237,58 @@ async def howToSearchAndSelect(ctx):
 
 @bot.command(help='Starts displaying the latest new images from the given links')
 async def startArtShow(ctx):
-    global keep_going 
-    keep_going = True
+    #global keep_going 
+    #keep_going = True
 
-    asyncio.create_task(artShow(ctx))
+    #asyncio.create_task(artShow(ctx))
+    cnx = mysql.connector.connect(
+    host=host,
+    user=user_2,
+    password=password
+    )
 
-    while keep_going:
-        documts = db.collection('character_threads')
-        doc_ref = documts.stream()
+    # while keep_going:
+    #     documts = db.collection('character_threads')
+    #     doc_ref = documts.stream()
 
-        opt = webdriver.ChromeOptions()
-        opt.add_argument("--incognito")
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
-        wait = WebDriverWait(driver, 20)
+    #     opt = webdriver.ChromeOptions()
+    #     opt.add_argument("--incognito")
+    #     driver = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
+    #     wait = WebDriverWait(driver, 20)
  
-        async for doc in doc_ref:
-            try:
-                d = doc.to_dict()
-                driver.get(d.get('link'))
+    #     async for doc in doc_ref:
+    #         try:
+    #             d = doc.to_dict()
+    #             driver.get(d.get('link'))
         
-                wait.until(EC.visibility_of_element_located((By.ID, 'post-list')))
-                post_list = driver.find_element(By.ID, 'post-list')
-                post = post_list.find_element(By.XPATH, f'div[2]/div[1]/span[1]/a')
-                post.click()
+    #             wait.until(EC.visibility_of_element_located((By.ID, 'post-list')))
+    #             post_list = driver.find_element(By.ID, 'post-list')
+    #             post = post_list.find_element(By.XPATH, f'div[2]/div[1]/span[1]/a')
+    #             post.click()
         
-                wait.until(EC.visibility_of_element_located((By.ID, 'right-col')))
-                img_spot = driver.find_element(By.ID, 'right-col')
-                img = img_spot.find_element(By.XPATH, f'div/img')
-                img_URL = img.get_attribute('src')
-                if img_URL == d.get('image'):
-                    print('Image already posted')
-                else:
-                    await documts.document(doc.id).update({'image': img_URL})
-                    print('Downloading image %s...' % (img_URL))
-                    urllib.request.urlretrieve(img_URL, os.getcwd() + '/image.jpg')
-                    channel = bot.get_channel(d.get('thread_id'))
-                    await channel.send('', file=discord.File('image.jpg'))
-            except (NoSuchElementException, JavascriptException):
-                print('Page error occured')
-        driver.close()
-        await asyncio.sleep(1)
+    #             wait.until(EC.visibility_of_element_located((By.ID, 'right-col')))
+    #             img_spot = driver.find_element(By.ID, 'right-col')
+    #             img = img_spot.find_element(By.XPATH, f'div/img')
+    #             img_URL = img.get_attribute('src')
+    #             if img_URL == d.get('image'):
+    #                 print('Image already posted')
+    #             else:
+    #                 await documts.document(doc.id).update({'image': img_URL})
+    #                 print('Downloading image %s...' % (img_URL))
+    #                 urllib.request.urlretrieve(img_URL, os.getcwd() + '/image.jpg')
+    #                 channel = bot.get_channel(d.get('thread_id'))
+    #                 await channel.send('', file=discord.File('image.jpg'))
+    #         except (NoSuchElementException, JavascriptException):
+    #             print('Page error occured')
+    #     driver.close()
+    #    await asyncio.sleep(1)
 
 @bot.command(help='Stops the bot from searching for new images to post')
 async def stopArtShow(ctx):
     cnx = mysql.connector.connect(
-    host=ep,
-    user=user,
-    password=ps
+    host=host,
+    user=user_2,
+    password=password
     )
     
     #global keep_going 
@@ -199,7 +296,7 @@ async def stopArtShow(ctx):
     if cnx.is_connected():
         cursor = cnx.cursor()
         cursor.execute(f"USE server_{ctx.guild.id};")
-        sql = f"UPDATE settings SET In_process = 0 WHERE ID = 1"
+        sql = f"UPDATE settings SET In_process = 0 WHERE Id = 1"
         cursor.execute(sql)
         cnx.commit()
 
